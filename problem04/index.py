d265c122bbbfc468f1be8be85d12cbf858aa3c4a
unknown <huangweiurob@gmail.com>
2018-12-28 12:34:25 +0800

    add a new solution of pro04

--- problem04/index.py
+++ problem04/index.py
@@ -3,11 +3,34 @@ def find_first_missing_integer(num_list):
     num_list = sorted(num_list)
 
     for i in range(len(num_list)):
-        if i + 1 == len(num_list):
-            if num_list[-1] < 0:
-                return 0
-            return num_list[-1] + 1
+        if i + 1 == len(num_list): #只有一个数时，或者数组的查找元素在数组之外时
+            if num_list[-1] <= 0: 
+                return 1
+            elif i == 0:
+                return 2 if num_list[-1] == 1 else 1
+            return num_list[i] + 1
         elif num_list[i] >= 0 and num_list[i + 1] - num_list[i] > 1:
             return num_list[i] + 1
         elif num_list[i] <= 0 and num_list[i + 1] > 0 and num_list[i + 1] != 1:
             return 1
+
+def find_first_missing_integer_beta(num_list):
+    max_num = max(num_list) #找到最大的值
+
+    if max_num < 1: #如果数列都是非正数,返回1
+        return 1
+
+    if len(num_list) == 1: #如果数列只有一个元素，那么返回2或者1，数列非1就返回1
+        return 1 if num_list[0] == 2 else 1
+    
+    tag_list = [0] * max_num
+
+    for num in num_list:
+        if num > 0 and tag_list[num - 1] == 0:
+            tag_list[num - 1] = 1
+    
+    
+    for index in range(len(tag_list)):
+        if tag_list[index] == 0:
+            return index + 1
+    return index + 2 
--- problem04/test.py
+++ problem04/test.py
@@ -1,5 +1,5 @@
 import unittest
-from index import find_first_missing_integer
+from index import find_first_missing_integer, find_first_missing_integer_beta
 
 class TestResult(unittest.TestCase):
     def test_example_case(self):
@@ -12,7 +12,7 @@ class TestResult(unittest.TestCase):
         result_1 = find_first_missing_integer([3, 3, 3, 4, 5, 7])
         result_2 = find_first_missing_integer([-1, -2, -2])
         self.assertEqual(result_1, 6)
-        self.assertEqual(result_2, 0)
+        self.assertEqual(result_2, 1)
 
     def test_mutiple_missing(self):
         result = find_first_missing_integer([-3 , -1, 1, 3, 5, 7, 9])
@@ -25,6 +25,20 @@ class TestResult(unittest.TestCase):
         self.assertEqual(result_1, 1)
         self.assertEqual(result_2, 4)
         self.assertEqual(result_3, 2)
+
+    def test_beta_func(self):
+        result_1 = find_first_missing_integer_beta([2, 3, 7, 6, 8, -1, -10, 15])
+        result_2 = find_first_missing_integer_beta([2, 3, -7, 6, 8, 1, -10, 15])
+        result_3 = find_first_missing_integer_beta([1, 1, 0, -1, -2])
+        result_4 = find_first_missing_integer([-3 , -1, 1, 3, 5, 7, 9])
+        result_5 = find_first_missing_integer([3, 4, -1, 1])
+        result_6= find_first_missing_integer([1, 2, 0])
+        self.assertEqual(result_1, 1)
+        self.assertEqual(result_2, 4)
+        self.assertEqual(result_3, 2)
+        self.assertEqual(result_4, 2)
+        self.assertEqual(result_5, 2)
+        self.assertEqual(result_6, 3)
 
 if __name__ == '__main__':
     unittest.main()
