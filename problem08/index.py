c3281b7567e281c30e796ff046098746c5059382
unknown <huangweiurob@gmail.com>
2019-01-03 12:55:05 +0800

    finished problem08

--- problem08/index.py
+++ problem08/index.py
@@ -1,2 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
 def count_unival_tree(root):
-    
+    if root == None:
+        return 0
+    if root.left == None and root.right == None:
+        return 1
+    total_count = count_unival_tree(root.left) + count_unival_tree(root.right) 
+    if root.left == None or root.right == None:
+        if (root.left and root.left.data == root.data) or (root.right and root.right.data == root.data):
+            total_count += 1
+    elif root.data == root.right.data == root.left.data:
+        total_count += 1
+    return total_count
--- problem08/test.py
+++ problem08/test.py
@@ -1,6 +1,19 @@
 import unittest
+from index import Node, count_unival_tree
 
+root_example = Node(0)
+root_example.left = Node(1)
+root_example.right = Node(0)
+root_example.right.left = Node(1)
+root_example.right.right = Node(0)
+root_example.right.left.left = Node(1)
+root_example.right.left.right = Node(1)
+root_example.right.left.right.right = Node(1)
+root_example.right.left.right.right.right = Node(1)
+root_example.right.left.right.right.right.right = Node(1)
 class TestResult(unittest.TestCase):
+    def test_example_case(self):
+        self.assertEqual(8, count_unival_tree(root_example))
 
 if __name__ == '__main__':
     unittest.main()
